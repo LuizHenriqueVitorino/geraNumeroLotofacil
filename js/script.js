054a1f8f4e114d74ef1b0c9636eab81044edbef0
Lucas Rocha <lr359508@gmail.com>
2024-09-14 16:51:03 -0300

    feat: adição de histórico de gerações

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ function gerarNumerosLotofacil() {
     const numeros = [];
     while (numeros.length < 15) {
         const numeroAleatorio = Math.floor(Math.random() * 25) + 1;
-        // Verifica se o número já está no conjunto, senão, então adiciona.
         if (!numeros.includes(numeroAleatorio)) {
             numeros.push(numeroAleatorio);
         }
@@ -14,11 +13,21 @@ function gerarNumerosLotofacil() {
 document.getElementById('gerar-btn').addEventListener('click', function() {
     const numeros = gerarNumerosLotofacil();
     const numerosDiv = document.querySelector('.numeros');
+    const historicoDiv = document.querySelector('.historico');
+
+    if (numerosDiv.firstChild) {
+        const historicoLine = document.createElement('div');
+        historicoLine.textContent = Array.from(numerosDiv.firstChild.children).map(el => el.textContent).join(', ');
+        historicoDiv.insertBefore(historicoLine, historicoDiv.firstChild);
+    }
+
     numerosDiv.innerHTML = '';
+    const lineDiv = document.createElement('div');
     numeros.forEach(numero => {
         const numeroSpan = document.createElement('span');
-        numeroSpan.className = 'numero'
+        numeroSpan.className = 'numero';
         numeroSpan.textContent = numero;
-        numerosDiv.appendChild(numeroSpan);
+        lineDiv.appendChild(numeroSpan);
     });
+    numerosDiv.appendChild(lineDiv);
 });
